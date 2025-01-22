6b56d7df5a4800bcb95981dde290253205d2905b
FacuMM <127054024+FacuMM@users.noreply.github.com>
2025-01-22 22:35:11 +0100

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,17 +1,26 @@
 import sqlite3
 
-conn = sqlite3.connect("database.db")
-cursor = conn.cursor()
 
-cursor.execute("""
-CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    username TEXT NOT NULL UNIQUE,
-    password TEXT NOT NULL
-)
-""")
 
-conn.commit()
-conn.close()
+def crear_tabla():
+    #Intentamos crar la tabla desde controles
+    try:
+        conn = sqlite3.connect("database.db")
+        cursor = conn.cursor()
 
+        cursor.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            username TEXT NOT NULL UNIQUE,
+            password TEXT NOT NULL
+        )
+        """)
+
+        conn.commit()
+        conn.close()
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        conn.close()
 print("Base de datos inicializada correctamente")
