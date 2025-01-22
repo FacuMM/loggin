886fcbc5aa5cd0709d1763d97a726b6ce2fb4d53
FacuMM <127054024+FacuMM@users.noreply.github.com>
2025-01-22 23:21:05 +0100

    Update controles.py

--- controles.py
+++ controles.py
@@ -68,5 +68,5 @@ def login():
         return jsonify({"message": "Credenciales incorrectas"}), 401
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=True)
